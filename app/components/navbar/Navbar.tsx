3d4456442745c5699c14759d9bf83ce67c8950a7
Anthony Watson <anthonywatson@Anthonys-MacBook-Pro.local>
2023-10-02 11:20:55 -0400

    updated Navbar component

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,35 +1,36 @@
-"use client";
+import { SafeUser } from "@/app/types";
 
-import React from "react";
+import Categories from "./Categories";
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
-import { User } from "@prisma/client";
-import Categories from "./Categories";
 
 interface NavbarProps {
-  currentUser?: User | null;
+  currentUser?: SafeUser | null;
 }
 
 const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
-      <div className="py-4 border-b-[1px]">
+      <div
+        className="
+          py-4 
+          border-b-[1px]
+        "
+      >
         <Container>
           <div
             className="
-              flex
-              flex-row
-              items-center
-              justify-between
-              gap-3
-              md:gap-0
-            "
+            flex 
+            flex-row 
+            items-center 
+            justify-between
+            gap-3
+            md:gap-0
+          "
           >
-            <div className="pr-2">
-              <Logo />
-            </div>
+            <Logo />
             <Search />
             <UserMenu currentUser={currentUser} />
           </div>
