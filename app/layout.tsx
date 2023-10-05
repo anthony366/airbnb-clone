f2347e1e9dc5507249b678cdebe1e0ce8e8f9735
Anthony Watson <anthonywatson@Anthonys-MacBook-Pro.local>
2023-10-04 20:51:28 -0400

    updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,8 @@ import getCurrentUser from "./actions/getCurrentUser";
 
 const font = Nunito({ subsets: ["latin"] });
 
+export const dynamic = "force-dynamic";
+
 export const metadata: Metadata = {
   title: "Airbnb",
   description: "Airbnb clone",
