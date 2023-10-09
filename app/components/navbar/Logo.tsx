42e6c178e8a6c46d016cdff9dd3d0ad19716606a
Anthony Watson <anthonywatson@Anthonys-MacBook-Pro.local>
2023-10-09 19:28:19 -0400

    updated logo size and search button

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -5,14 +5,25 @@ import { useRouter } from "next/navigation";
 const Logo = () => {
   const router = useRouter();
   return (
-    <Image
-      onClick={() => router.push("/")}
-      alt="Logo"
-      className="hidden md:block cursor-pointer"
-      height="40"
-      width="128"
-      src="/images/online-bnb-logo.png"
-    />
+    <>
+      <Image
+        onClick={() => router.push("/")}
+        alt="Logo"
+        className="hidden lg:block cursor-pointer"
+        height="47"
+        width="175"
+        src="/images/online-bnb-logo.png"
+      />
+
+      <Image
+        onClick={() => router.push("/")}
+        alt="Logo"
+        className="hidden md:block cursor-pointer"
+        height="47"
+        width="43"
+        src="/images/online-bnb-brand.png"
+      />
+    </>
   );
 };
 
--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -112,7 +112,7 @@ const Search = () => {
           <div
             className="
               p-2 
-              bg-rose-500 
+              bg-blue-400
               rounded-full 
               text-white
             "
